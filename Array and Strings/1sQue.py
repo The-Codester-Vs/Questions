d1e37af91c0ad06f843c24469b60107c2beea0c9
The-Codester-Vs <viveksharma55236@gmail.com>
2020-12-27 21:25:14 +0530

    1st Question

--- Array and Strings/1sQue.py
+++ Array and Strings/1sQue.py
@@ -3,7 +3,22 @@
 # ! cannot use additional data structures?
 
 def unique_character(string):
-    char_set = [False]*128
+    if len(string) > 128:
+        return False
+    
+    character = [False] * 128
     for char in string:
         val = ord(char)
-        
+        if character[val]:
+            return False
+        character[val] = True
+
+    return True
+
+# unique_character("vivek")
+string = input("Enter the string: ")
+
+if unique_character(string):
+    print("This string have a unique character!")
+else:
+    print("This string does not have a unique character!")
