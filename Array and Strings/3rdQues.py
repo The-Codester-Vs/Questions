2c922060b689c6c7e46a67e6cb1718bd77abff9a
The-Codester-Vs <viveksharma55236@gmail.com>
2021-01-02 21:57:47 +0530

    3rd Question

--- Array and Strings/3rdQues.py
+++ Array and Strings/3rdQues.py
@@ -3,24 +3,24 @@
 # ! has sufficient space at the end to hold the additional characters, and that you are given the "true"
 # ! length of the string.
 
-# def replacee(string,lenght):
-#     chr_lst = list(string)
-#     string = ""
-#     index = len(chr_lst)
+def replaceing1(string,length):
+    chr_lst = list(string)
+    string = " "
+    index = len(chr_lst)
     
-#     for i in reversed(range(lenght)):
-#         if chr_lst[i] == " ":
-#             chr_lst[index - 3:index ] = "%20"
-#             index -= 3
-#         else:
-#             chr_lst[index-1] = chr_lst[i]
-#             index -= 1
+    for i in reversed(range(length)):
+        if chr_lst[i] == " ":
+            chr_lst[index - 2:index ] = "%20"
+            index -= 1
+        else:
+            chr_lst[index-1] = chr_lst[i]
+            index -= 1
     
-#     return string.join(chr_lst)
+    return string.join(chr_lst)
 
-def traling(text,lenght):
+def replacing2(text,length):
     return text.rstrip().replace(" ", "%20")
     
 ar = "hello world"
-print(traling(ar,len(ar)))
-            
+print(replaceing1(ar,len(ar)))
+print(replacing2(ar,len(ar)))
